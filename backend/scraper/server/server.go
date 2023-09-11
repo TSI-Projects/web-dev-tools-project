8d25946405c5f3510f391fe777db1ef7fb6fe132
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2023-09-11 20:35:30 +0300

    Allow headers

--- backend/scraper/server/server.go
+++ backend/scraper/server/server.go
@@ -23,6 +23,10 @@ func Start() {
 }
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 	values := r.URL.Query()
 
 	productName, found := values["product"]
