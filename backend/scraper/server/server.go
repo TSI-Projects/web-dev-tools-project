a5997b295a74ef00e7662442b158f83f9654b456
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2023-09-11 19:31:34 +0300

    Add CORS to the server

--- backend/scraper/server/server.go
+++ backend/scraper/server/server.go
@@ -15,6 +15,10 @@ func Start() {
 }
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 	values := r.URL.Query()
 
 	productName, found := values["product"]
