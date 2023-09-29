0780e2b2d56758ce1120540a88f42100b3ec6435
Vadim SkinonikS.# <skinnoniks@gmail.com>
2023-09-29 22:48:25 +0300

    Remove old fetch function from composable

--- frontend/composables/useProducts.ts
+++ frontend/composables/useProducts.ts
@@ -132,43 +132,6 @@ export default function () {
             close,
         };
     };
-
-    const fetchAll = (params: FetchAllParameters): Promise<Product[]> => {
-        return $fetch<Product[]>('/search', {
-            baseURL: publicConfig.api.baseUrl || 'http://localhost',
-            params: {
-                product: params.query.query,
-                sources: wrap<string, undefined>(params.query.sources, undefined),
-                categories: wrap<string, undefined>(params.query.categories, undefined),
-                price: {
-                    min: params.query.price.min,
-                    max: params.query.price.max,
-                },
-            },
-            onRequest: (ctx) => {
-                if (ctx.options.params || ctx.options.query) {
-                    ctx.request = ctx.request + qs.stringify({
-                        ...ctx.options.query,
-                        ...ctx.options.params,
-                    }, {
-                        arrayFormat: 'brackets',
-                        addQueryPrefix: true,
-                        skipNulls: true,
-                    });
-
-                    ctx.options.params = undefined;
-                    ctx.options.query = undefined;
-                }
-            },
-            headers: {
-                'accept': 'application/json',
-            },
-            // TODO: delete this when backend is ready
-            parseResponse(string) {
-                return JSON.parse(string);
-            },
-        });
-    };
     
     const fetchOne = () => {
         // TODO: implement when the backend is ready
@@ -176,7 +139,6 @@ export default function () {
 
     return {
         sseFetch,
-        fetchAll,
         fetchOne,
     };
 }
