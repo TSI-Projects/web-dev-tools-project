34fd656215cf6e93ae52eee70082088b9d6b20da
Vadim SkinonikS.# <skinnoniks@gmail.com>
2023-10-13 00:39:23 +0300

    Rename index.g.ts to global.d.ts

---
+++ frontend/global.d.ts
@@ -0,0 +1,28 @@
+import type { OpenGraph, TwitterCard } from "./composables/useSeoMetaFactory";
+
+declare module 'nuxt/schema' {
+    // App config
+    interface AppConfigInput {
+        appName: string;
+        sourceCodeLink: string;
+        footerAboutUs: string;
+    }
+
+    interface AppConfig {
+        appName: string;
+        sourceCodeLink: string;
+        footerAboutUs: string;
+    }
+
+    // Runtime config
+    interface PublicRuntimeConfig {
+        app: {
+            rootUrl: string;
+        }
+        api: {
+            baseUrl: string;
+        }
+    }
+}
+
+export { };
